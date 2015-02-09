5fa5deb87e5d36167460a3ac07814802167beed7
gie3d <gie68i@gmail.com>
2015-02-09 21:46:34 +0700

    make it more mvc

--- autosuggest-angular/js/app.js
+++ autosuggest-angular/js/app.js
@@ -1,102 +1,103 @@
 (function(){
-	var selectedResult = {
-		"list": [
-			{
-				"ric": "005930.KS",
-				"title": "Samsung Electronics Co Ltd"
-			},
-			{
-				"ric": "601857.SS",
-				"title": "PetroChina Co Ltd"
-			},
-			{
-				"ric": "7203.T",
-				"title": "Toyota Motor Corp"
-			},
-			{
-				"ric": "BHP.AX",
-				"title": "BHP Billiton Ltd"
-			},
-			{
-				"ric": "SIEGn.DE",
-				"title": "Siemens AG"
-			},
-			{
-				"ric": "TRI.TO",
-				"title": "Thomson Reuters Corp"
-			}
-		]
-	};
-
-	var dataSource = {
-		"rics": [
-			{
-				"ric": "IBM.N",
-				"subtitle": "Ordinary Share - New York Stock Exchange - IBM",
-				"title": "International Business Machines Corp",
-				"type": "EQ"
-			},
-			{
-				"ric": "A",
-				"subtitle": "Ordinary Share - NYSE Consolidated - A",
-				"title": "Agilent Technologies Inc",
-				"type": "EQ"
-			},
-			{
-				"ric": "GOOGL.O",
-				"subtitle": "Ordinary Share - NASDAQ Global Select Consolidated - GOOGL",
-				"title": "Google Inc",
-				"type": "EQ"
-			},
-			{
-				"ric": "AAPL.O",
-				"subtitle": "Ordinary Share - NASDAQ Global Select Consolidated - AAPL",
-				"title": "Apple Inc",
-				"type": "EQ"
-			},
-			{
-				"ric": "0#CL:",
-				"subtitle": "Crude Oil - Commodity Future - New York Mercantile Exchange (NYMEX)",
-				"title": "NYMEX Light Sweet Crude Oil (WTI) Composite Energy Future Chain Contracts",
-				"type": "COM"
-			},
-			{
-				"ric": "0#WTI:",
-				"subtitle": "Commodity Future - ROSARIO FUTURES EXCHANGE",
-				"title": "ROFEX WTI Light Sweet Crude Oil Energy Future Chain Contract",
-				"type": "COM"
-			},
-			{
-				"ric": "THB=",
-				"subtitle": "THOMSON REUTERS",
-				"title": "US Dollar/Thai Baht FX Spot Rate",
-				"type": "FX"
-			},
-			{
-				"ric": "AUD=",
-				"subtitle": "THOMSON REUTERS",
-				"title": "Australian Dollar/US Dollar FX Spot Rate",
-				"type": "FX"
-			},
-			{
-				"ric": ".SPX",
-				"subtitle": "US - Equity Index - MXP",
-				"title": "S&P 500 Index",
-				"type": "INDX"
-			},
-			{
-				"ric": "0#USBMK=",
-				"subtitle": "US BMK YC - THOMSON REUTERS",
-				"title": "United States Government Benchmark Yield Curve",
-				"type": "BMK"
-			}
-		]
-	};
 
 	var app = angular.module("autoSuggestApp",[]);
 
 	app.factory('autoSuggestFactory', function(){
-		var displayedRics = selectedResult;
+
+		var displayedRics = {
+			"list": [
+				{
+					"ric": "005930.KS",
+					"title": "Samsung Electronics Co Ltd"
+				},
+				{
+					"ric": "601857.SS",
+					"title": "PetroChina Co Ltd"
+				},
+				{
+					"ric": "7203.T",
+					"title": "Toyota Motor Corp"
+				},
+				{
+					"ric": "BHP.AX",
+					"title": "BHP Billiton Ltd"
+				},
+				{
+					"ric": "SIEGn.DE",
+					"title": "Siemens AG"
+				},
+				{
+					"ric": "TRI.TO",
+					"title": "Thomson Reuters Corp"
+				}
+			]
+		};
+
+		var dataSource = {
+			"rics": [
+				{
+					"ric": "IBM.N",
+					"subtitle": "Ordinary Share - New York Stock Exchange - IBM",
+					"title": "International Business Machines Corp",
+					"type": "EQ"
+				},
+				{
+					"ric": "A",
+					"subtitle": "Ordinary Share - NYSE Consolidated - A",
+					"title": "Agilent Technologies Inc",
+					"type": "EQ"
+				},
+				{
+					"ric": "GOOGL.O",
+					"subtitle": "Ordinary Share - NASDAQ Global Select Consolidated - GOOGL",
+					"title": "Google Inc",
+					"type": "EQ"
+				},
+				{
+					"ric": "AAPL.O",
+					"subtitle": "Ordinary Share - NASDAQ Global Select Consolidated - AAPL",
+					"title": "Apple Inc",
+					"type": "EQ"
+				},
+				{
+					"ric": "0#CL:",
+					"subtitle": "Crude Oil - Commodity Future - New York Mercantile Exchange (NYMEX)",
+					"title": "NYMEX Light Sweet Crude Oil (WTI) Composite Energy Future Chain Contracts",
+					"type": "COM"
+				},
+				{
+					"ric": "0#WTI:",
+					"subtitle": "Commodity Future - ROSARIO FUTURES EXCHANGE",
+					"title": "ROFEX WTI Light Sweet Crude Oil Energy Future Chain Contract",
+					"type": "COM"
+				},
+				{
+					"ric": "THB=",
+					"subtitle": "THOMSON REUTERS",
+					"title": "US Dollar/Thai Baht FX Spot Rate",
+					"type": "FX"
+				},
+				{
+					"ric": "AUD=",
+					"subtitle": "THOMSON REUTERS",
+					"title": "Australian Dollar/US Dollar FX Spot Rate",
+					"type": "FX"
+				},
+				{
+					"ric": ".SPX",
+					"subtitle": "US - Equity Index - MXP",
+					"title": "S&P 500 Index",
+					"type": "INDX"
+				},
+				{
+					"ric": "0#USBMK=",
+					"subtitle": "US BMK YC - THOMSON REUTERS",
+					"title": "United States Government Benchmark Yield Curve",
+					"type": "BMK"
+				}
+			]
+		};
+
 		var currentPage = 0;
 		var pageSize = 5;
 
@@ -114,6 +115,9 @@
 		}
 		var getDisplayedRics = function(){
 			return displayedRics;
+		}
+		var getDataSource = function(){
+			return dataSource;
 		}
 		var isRicDup = function(ricnumber){
 			var isDup = false,
@@ -151,21 +155,27 @@
 			currentPage: currentPage,
 			pageSize: pageSize,
 			totalPages: totalPages,
-			getCurrentPage: getCurrentPage
+			getCurrentPage: getCurrentPage,
+			getDataSource: getDataSource
 		}
 	});
 
 	app.controller('autoSugesstCtrl', ['$scope', 'autoSuggestFactory', function($scope, autoSuggestFactory){
+		
 		$scope.searchRic = function(){
+			
 			$scope.duplicateRicAlert = false;
+
+			var dataSource = autoSuggestFactory.getDataSource();
 			$scope.suggests = [];
 			var stext = $scope.searchText.toLowerCase();
-			//console.log(stext)
+			
 			for(var i=0,l=dataSource.rics.length;i<l;i++){
 				if ((dataSource.rics[i].ric.toLowerCase().indexOf(stext) !== -1 || dataSource.rics[i].subtitle.toLowerCase().indexOf(stext) !== -1 || dataSource.rics[i].title.toLowerCase().indexOf(stext) !== -1 || dataSource.rics[i].type.toLowerCase().indexOf(stext) !== -1) && $scope.searchText.length > 0){
 					$scope.suggests.push(dataSource.rics[i]);
 				}
 			}
+
 			if($scope.suggests.length > 0){
 				$scope.displaySearchBox = true;
 				$scope.searchTextBox = "form-group";
@@ -198,6 +208,7 @@
 	}]);
 
 	app.controller('selectedRicsCtrl', ['$scope', 'autoSuggestFactory', function($scope, autoSuggestFactory){
+		
 		$scope.displayedRics = autoSuggestFactory.getDisplayedRics();
 		$scope.pageSize = autoSuggestFactory.pageSize;		
 
@@ -209,8 +220,8 @@
 			return autoSuggestFactory.totalPages();
 		};
 
-		$scope.setPage = function(){
-			autoSuggestFactory.setPage(this.n);
+		$scope.setPage = function(n){
+			autoSuggestFactory.setPage(n);
 		};
 
 		$scope.nextPage = function(){
